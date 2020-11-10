a0df44aca2c7502248cfeb2e98c6dc59775cc86b
Kushagra5678 <kushu.7feb@gmail.com>
2020-11-10 09:10:04 +0530

    no alphabets at 1st place

--- PincodeValidation.js
+++ PincodeValidation.js
@@ -1,6 +1,4 @@
-
-  
-const pinRegex = RegExp("^\\S{6}$");
+const pinRegex = RegExp("^([1-9])(\\S){5}$");
 
 function pinCodeTest(pinCode){
     if(pinRegex.test(pinCode))console.log("Valid Pin Code");
@@ -8,7 +6,7 @@ function pinCodeTest(pinCode){
 }
 
 try{
-    pinCodeTest("400088");
+    pinCodeTest("a40088");
 }catch(e){
     console.error(e);
 }
