13b059075cffdc64ab1cf8699fdde4249700434c
Kushagra5678 <kushu.7feb@gmail.com>
2020-11-10 09:05:14 +0530

    pincode validation

---
+++ PincodeValidation.js
@@ -0,0 +1,14 @@
+
+  
+const pinRegex = RegExp("^\\S{6}$");
+
+function pinCodeTest(pinCode){
+    if(pinRegex.test(pinCode))console.log("Valid Pin Code");
+    else throw "Pin Code is Invalid";
+}
+
+try{
+    pinCodeTest("400088");
+}catch(e){
+    console.error(e);
+}
